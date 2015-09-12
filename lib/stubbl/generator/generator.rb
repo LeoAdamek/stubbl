c19c3c3f2baa4933719019d44de0f7dbb20fe814
Leo Adamek <leo@adamek.me>
2015-09-12 23:40:01 +0100

    Added a skeleton for the generator

--- lib/stubbl/generator/generator.rb
+++ lib/stubbl/generator/generator.rb
@@ -1,6 +1,14 @@
 module Stubbl
   class Generator
-    
-    
+    class << self
+
+      ##
+      # Generate the stub for an issue
+      #
+      # @param [Stubbl::JIRAIssue] issue JIRA Issue
+      def generate(issue)
+        p issue.self
+      end
+    end
   end
 end
