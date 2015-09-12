c0f9a79f67d3c32211cf8adf8e96afef2eff15a3
Leo Adamek <leo@adamek.me>
2015-09-12 23:40:33 +0100

    Added a simple CLI task for an issue

--- lib/stubbl/cli/cli.rb
+++ lib/stubbl/cli/cli.rb
@@ -1,7 +1,20 @@
-module Stubbl
-  class CLI < Thor
 
-    
-        
+module Stubbl
+  module CLI
+    class App < Thor
+
+      desc 'issue [KEY]', 'Generate the stub for issue KEY'
+      map 'i' => :issue
+      def issue(issue_key)
+        jira = Stubbl::JIRA.new( ENV['JIRA_URL'], ENV['JIRA_USER'], ENV['JIRA_PASS'] )
+        issue = jira.issue issue_key
+
+        Stubbl::Generator.generate issue
+      end
+      
+    end
   end
 end
+
+require 'stubbl/lib/jira'
+Stubbl.load_generator
