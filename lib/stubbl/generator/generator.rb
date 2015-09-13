c2f89bb1e65ae737b818ab56e4a44fd85ef16661
Leo Adamek <leo@adamek.me>
2015-09-13 17:58:57 +0100

    Changed font sizes to points (because print is stupid)

--- lib/stubbl/generator/generator.rb
+++ lib/stubbl/generator/generator.rb
@@ -6,14 +6,19 @@ module Stubbl
       ##
       # Generate the stub for a single issue
       #
-      # @param [Stubbl::JIRAIssue] issue JIRA Issue
-      def issue(issue)
+      # @param [String] issue JIRA Issue Key
+      def issue(issue_key)
+        jira = Stubbl::JIRA.new( ENV['JIRA_URL'], ENV['JIRA_USER'], ENV['JIRA_PASS'] )
+        issue = jira.issue issue_key
+        
         doc = Prawn::Document.new(
           page_size: [19.mm, 53.mm],
           page_layout: :landscape,
           margin: 2.mm
         )
 
+        doc.font "Courier"
+        
         page_for_issue( doc, issue )
 
         doc
@@ -22,17 +27,20 @@ module Stubbl
       ##
       # Generate the stubs for multiple issues
       #
-      # @param [Array[Stubbl::JIRAIssue]] issues JIRA Issues
+      # @param [Array[String]] issues JIRA Issue Keys
       def issues(issues)
+
+        jira = Stubbl::JIRA.new( ENV['JIRA_URL'], ENV['JIRA_USER'], ENV['JIRA_PASS'] )
+        
         doc = Prawn::Document.new(
           page_size: [19.mm, 53.mm],
           page_layout: :landscape,
-          margin: (1.5).mm
+          margin: 1.mm
         )
 
         issues.each do |issue|
           doc.start_new_page
-          page_for_issue(doc, issue)
+          page_for_issue(doc, jira.issue(issue))
         end
 
         doc
@@ -49,26 +57,26 @@ module Stubbl
 
         # Add the issue key
         doc.draw_text issue[:key],
-                      at: [5.mm, 12.mm],
-                      size: 4.mm
+                      at: [3.mm , (12.5).mm],
+                      size: 14
 
         # Add the type
         doc.draw_text issue.fields.issuetype.name[0].upcase,
-                      at: [0, 12.mm],
-                      size: 5.mm
+                      at: [0, (12.5).mm],
+                      size: 10
         
         # Draw the top-line
         doc.horizontal_line (-2).mm, 53.mm, at: 12.mm
         
         # Add the summary
-        doc.draw_text issue.fields.summary,
-                      at: [0, 8.mm],
-                      size: 2.mm
+        doc.move_down 5.mm
+        doc.text issue.fields.summary,
+                      size: 8
 
         # Add the creation date so we know if the issue is stale.
         doc.draw_text (Time.parse issue.fields.created).strftime('%d/%m/%y'),
-                      at: [0, (0.5).mm],
-                      size: (2.5).mm
+                      at: [0, 1],
+                      size: 10
         
         # Add a QR code which links to the issue
         qrcode = RQRCode::QRCode.new(issue.self)
