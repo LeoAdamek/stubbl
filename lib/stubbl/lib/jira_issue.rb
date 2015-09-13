bdfc8db551896de4814194ef53951923e5eb013a
Leo Adamek <leo@adamek.me>
2015-09-13 21:42:05 +0100

    Change JIRAIssue#icon to use an image

--- lib/stubbl/lib/jira_issue.rb
+++ lib/stubbl/lib/jira_issue.rb
@@ -1,16 +1,17 @@
+# coding: utf-8
 module Stubbl
   class JIRAIssue < Hashie::Mash
-    
-      ##
-      # Get the icon file for this issue's type
-      #
-      # @return [String] icon file path
-      def icon
-        s = fields.issuetype.name.downcase.gsub(/\W+/, '-')
-        sp = File.expand_path(File.dirname(__FILE__) + '/../icons/' + s + '.png')
+    ##
+    # Get the icon file for this issue's type
+    #
+    # @return [String] icon file path
+    def icon
+      s = fields.issuetype.name.downcase.gsub(/\W+/, '_')
+      ip = File.expand_path( File.dirname(__FILE__) + '/../icons/' + s + '.png')
 
-        return sp if File.exists? sp
-        nil
-      end
+      return ip if File.exists? ip
+
+      s[0].upcase
+    end
   end
 end
