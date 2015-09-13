ad25db5e4f2e47b98e8f4984a17278bf78b8516a
Leo Adamek <leo@adamek.me>
2015-09-13 17:59:52 +0100

    Added #icon to issues

--- lib/stubbl/lib/jira_issue.rb
+++ lib/stubbl/lib/jira_issue.rb
@@ -1,5 +1,16 @@
 module Stubbl
   class JIRAIssue < Hashie::Mash
+    
+      ##
+      # Get the icon file for this issue's type
+      #
+      # @return [String] icon file path
+      def icon
+        s = fields.issuetype.name.downcase.gsub(/\W+/, '-')
+        sp = File.expand_path(File.dirname(__FILE__) + '/../icons/' + s + '.png')
 
+        return sp if File.exists? sp
+        nil
+      end
   end
 end
