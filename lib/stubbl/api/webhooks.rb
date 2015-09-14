c6418ed006a7d18a99594b23ba8d0db86cd36a44
Leo Adamek <leo.adamek@mrzen.com>
2015-09-14 16:01:40 +0100

    Added soem code to the atlassian-connect descriptor

--- lib/stubbl/api/webhooks.rb
+++ lib/stubbl/api/webhooks.rb
@@ -5,8 +5,20 @@ module Stubbl
       desc 'Get the Atlassian Connect description'
       get '/atlassian-connect' do
         {
-          message: 'Not yet implemented'
+          name: 'Stubbl',
+          description: 'Ticket stubs have never been this good.',
+          baseUrl: 'https://3ebe5e4b.ngrok.com/',
+          key: 'com.mrzen.stubbl',
+          vendor: {
+            name: 'Mr. Zen, Ltd.',
+            url: 'http://mrzen.com'
+          },
 
+          authentication: {
+            type: 'none'
+          },
+
+          version: '0.1'
         }
       end
       
